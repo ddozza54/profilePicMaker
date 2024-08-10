9ce4202249666f5d1c9b619e4d5badc07c72283e
ddozza54 <bananamayong@gmail.com>
2024-08-10 21:41:25 +0900

    ✨ File input 추가로 image 로드 가능

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const canvas = document.querySelector('canvas');
 const lineWidth = document.getElementById('line-width');
 const color = document.getElementById('color');
+const fileInput = document.getElementById('file');
 const context = canvas.getContext('2d');
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 context.lineWidth = lineWidth.value;
 
 let isPainting = false;
@@ -36,9 +40,27 @@ const onColorChange = (event) => {
   context.fillStyle = event.target.value;
 };
 
+const onFileChange = (event) => {
+  const file = event.target.files[0];
+  const url = URL.createObjectURL(file);
+  const image = new Image();
+  image.src = url;
+  image.onload = function () {
+    context.drawImage(
+      image,
+      0,
+      0,
+      CANVAS_WIDTH,
+      CANVAS_HEIGHT
+    );
+    fileInput.value = null;
+  };
+};
+
 canvas.addEventListener('mousemove', onMouseMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener('change', onColorChange);
+fileInput.addEventListener('change', onFileChange);
