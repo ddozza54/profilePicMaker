c858899400c02206285605fe2ad41ef3b4a552c4
ddozza54 <bananamayong@gmail.com>
2024-09-29 22:59:27 +0900

    🎨 이미지 import문 정리

---
+++ ProfilePicMaker/src/constant/imgPaths.ts
@@ -0,0 +1,19 @@
+export const QUOKKA_IMG_SRC =
+  'public/imgs/character/quokka.PNG';
+export const BEAR_IMG_SRC =
+  'public/imgs/character/bear.PNG';
+export const RABBIT_IMG_SRC =
+  'public/imgs/character/rabbit.PNG';
+export const CHINCHILLA_IMG_SRC =
+  'public/imgs/character/chinchilla.PNG';
+export const DESK1_IMG_SRC = 'public/imgs/desk/desk_1.PNG';
+export const DESK2_IMG_SRC = 'public/imgs/desk/desk_2.PNG';
+export const DESK3_IMG_SRC = 'public/imgs/desk/desk_3.PNG';
+export const DRINK1_IMG_SRC =
+  'public/imgs/drink/drink_1.PNG';
+export const DRINK2_IMG_SRC =
+  'public/imgs/drink/drink_2.PNG';
+export const LAPTOP1_IMG_SRC =
+  'public/imgs/laptop/laptop_1.PNG';
+export const LAPTOP2_IMG_SRC =
+  'public/imgs/laptop/laptop_2.PNG';
